d270d7818a34b5e511f516fbe5b790c720000ea0
winnerdinner701 <80855037+winnerdinner701@users.noreply.github.com>
2023-09-11 19:24:50 +0100

    Update kill.py

--- kill.py
+++ kill.py
@@ -2,3 +2,7 @@ import os
 os.system('taskkill /f /im veyon-worker.exe')
 os.system('taskkill /f /im veyon-server.exe')
 os.system('taskkill /f /im veyon-service.exe')
+
+# taskkill /f /im veyon-worker.exe
+# taskkill /f /im veyon-server.exe
+# taskkill /f /im veyon-service.exe
